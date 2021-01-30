1b445bed8ec32253808502256e58e97363f23498
uc-leidenbn <leidenbn@mail.uc.edu>
2021-01-30 01:21:05 -0500

    adding powershell files

--- powershell/sysinfo.ps1
+++ powershell/sysinfo.ps1
@@ -1,5 +1,17 @@
 ﻿function getIP{
-    (Get-NetIPAddress).IPv4Address | Select-String "192*"
+    (Get-NetIPAddress).ipv4address | Select-String "192*"
 }
 
-write-host(getIP)
+function getUser{
+    (whoami)
+}
+
+$IP=getIP
+$getuser=getUser
+$gethostname=$env:COMPUTERNAME
+$getDate=Get-Date -UFormat "%A, %B %e, %Y"
+$Psversion=$Host.Version.Major
+
+$mailbody="This machine's IP is $IP. User is $getuser. Hostname is $gethostname. Powershell Version $psversion. Today's Date is $getDate."
+
+Send-MailMessage -To "leidenbn@mail.uc.edu" -From "us1-hosting@gigabition.com" -Subject "IT3038C Windows Sysinfo" -Body $mailbody -SmtpServer smtp.office365.com -port 587 -UseSsl -Credential (Get-Credential)
