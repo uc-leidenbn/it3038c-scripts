a962c8815539df6958d594062f26e09591473b78
uc-leidenbn <77517978+uc-leidenbn@users.noreply.github.com>
2021-02-15 00:15:06 -0500

    Update Lab5.py

--- Labs/Lab5.py
+++ Labs/Lab5.py
@@ -1,1 +1,25 @@
-#Lab5
+#Lab5 leidenbn
+
+import datetime 
+
+print('What is your year of Birth?')
+yearofbirth = input()
+yearofbirth = int(yearofbirth)
+
+print('What is your month of Birth?')
+monthofbirth = input()
+monthofbirth = int(monthofbirth)
+
+print('What is your day of Birth?')
+dayofbirth = input()
+dayofbirth = int(dayofbirth)
+
+now = datetime.datetime.now() 
+dob = datetime.datetime(yearofbirth, monthofbirth, dayofbirth) 
+
+difference = now-dob 
+seconds = difference.total_seconds()
+
+print('You are this many seconds old: ', seconds) 
+
+
