50bcd4204e187b44e53157be28fd12d4d733c706
uc-leidenbn <77517978+uc-leidenbn@users.noreply.github.com>
2021-02-14 23:05:17 -0500

    Update nameage.py

--- python/nameage.py
+++ python/nameage.py
@@ -1,1 +1,6 @@
 #NameAge Script
+print('What is your name?')
+myName = input()
+print('Hello, ' + myName + '. That is a good name. How old are you?')
+myAge = input()
+print (myAge + "? That' funny, I'm only a few seconds old")
