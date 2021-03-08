e5c6d5f094f266684241791efc91aa2982ecfff0
uc-leidenbn <77517978+uc-leidenbn@users.noreply.github.com>
2021-03-07 19:01:14 -0500

    Lab7

---
+++ Labs/Lab7/Lab7.ps1
@@ -0,0 +1,18 @@
+﻿#Getting Detailed Information about a user's devices, manager, and applied licenses. 
+
+Connect-AzureAD
+#Log in using the GUI pop-up. 
+
+$userprincipalname = 'jim@gigabition.com'
+#specify user's UPN here, example shown. 
+
+
+Get-AzureADUserRegisteredDevice -ObjectID "$userprincipalname"
+#Fetches information about any devices a user has registered, includes version, hostname, etc. 
+
+Get-AzureADUserManager -ObjectID "$userprincipalname"
+#Fetches the user's manager information, includes name, UPN, etc. 
+
+Get-AzureADUserLicenseDetail -ObjectID "$userprincipalname"
+#Fetches applied licenses, example contains Business Standard & Power Automate. 
+
